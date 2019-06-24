a77f2414ba2a86d80f72f4961bda178f7a6b716f
VicLopes <viclopes2621@gmail.com>
2019-06-24 08:50:17 -0300

    laura
    victor

---
+++ sketch_jun23a/sketch_jun23a.ino
@@ -0,0 +1,82 @@
+#include <EtherCard.h>
+
+// ethernet interface mac address, must be unique on the LAN
+byte mymac[] = { 0x74,0x69,0x69,0x2D,0x30,0x31 };
+
+const char website[] PROGMEM = "ticket-project.herokuapp.com";
+
+static byte session;
+byte Ethernet::buffer[700];
+Stash stash;
+
+char qrcode[] = "90b1ba8e-2f8b-48c1-aa5a-934200316df9";
+char oldcode[36];
+void setup () {
+  Serial.begin(57600);
+  
+  if (ether.begin(sizeof Ethernet::buffer, mymac) == 0) 
+     Serial.println(F("Failed to access Ethernet controller"));
+  if (!ether.dhcpSetup())
+     Serial.println(F("DHCP failed"));
+  
+  ether.printIp("IP:  ", ether.myip);
+  ether.printIp("GW:  ", ether.gwip);  
+  ether.printIp("DNS: ", ether.dnsip);  
+  
+  if (!ether.dnsLookup(website))
+     Serial.println(F("DNS failed"));
+  
+  ether.printIp("SRV: ", ether.hisip);
+  
+  getReq();
+
+}
+
+void loop () {
+  
+  ether.packetLoop(ether.packetReceive());
+  
+  const char* reply = ether.tcpReply(session);
+  if (reply != 0) {
+    Serial.println(reply);
+  }
+
+  
+}
+
+
+static void getReq () {
+  Serial.print("Sending QR code:");
+  Serial.println(qrcode);
+  byte sd = stash.create();
+  
+  stash.print("qr_code=");
+  stash.println(qrcode);
+  stash.save();
+  int stash_size = stash.size();
+  
+  
+  Stash::prepare(PSTR("POST /api/tickets HTTP/1.1" 
+                      "\r\n"
+                      "Host: $F" "\r\n"
+                      "User-Agent: Arduino/1.0" "\r\n"
+                      "Content-Length: $D" "\r\n"
+                      "Content-Type: application/x-www-form-urlencoded" "\r\n"
+                      "\r\n"
+                      "$H"),
+                      website, stash_size, sd);
+  
+  
+  
+  
+  // send the packet - this also releases all stash buffers once done
+  // Save the session ID so we can watch for it in the main loop.
+  session = ether.tcpSend();
+}
+
+char obterqr() {
+  Serial.readBytes(qrcode, 36);
+  Serial.print("Leitura completa do codigo: ");
+  Serial.println(qrcode);
+  delay(1000);
+}
---
+++ xively/xively.ino
@@ -0,0 +1,134 @@
+// Simple demo for feeding some random data to Pachube.
+// 2011-07-08 <jc@wippler.nl>
+//
+// License: GPLv2
+
+// Handle returning code and reset ethernet module if needed
+// 2013-10-22 hneiraf@gmail.com
+
+#include <EtherCard.h>
+#include <ArduinoJson.h>
+
+// change these settings to match your own setup
+#define FEED "000"
+#define APIKEY "xxx"
+
+// ethernet interface mac address, must be unique on the LAN
+static byte mymac[] = { 0x74,0x69,0x69,0x2D,0x30,0x31 };
+
+const char website[] PROGMEM = "ticket-project.herokuapp.com";
+
+byte Ethernet::buffer[700];
+uint32_t timer;
+Stash stash;
+byte session;
+
+//timing variable
+int res = 0;
+
+char qrcode[] = "1df709c5-2458-4ae7-a471-ba1c66b0e0cd";
+
+char obterqr() {
+  Serial.readBytes(qrcode, 36);
+  Serial.print("Leitura completa do codigo: ");
+  Serial.println(qrcode);
+  delay(1000);
+}
+
+void setup () {
+  Serial.begin(57600);
+  Serial.println("\n[Ticket Example]");
+
+  //Initialize Ethernet
+  initialize_ethernet();
+}
+
+
+void loop () {
+
+  //if correct answer is not received then re-initialize ethernet module
+  if (res > 220){
+    initialize_ethernet();
+  }
+
+  res = res + 1;
+
+  ether.packetLoop(ether.packetReceive());
+
+  //200 res = 10 seconds (50ms each res)
+  if (res == 200) {
+    StaticJsonDocument<61> doc;
+    obterqr();
+    doc["qr_code"].set(qrcode);
+    Serial.print("Sending QR code:");
+    Serial.println(qrcode);
+    byte sd = stash.create();
+    stash.print("qr_code=");
+    stash.println(qrcode);
+    stash.save();
+    int stash_size = stash.size();
+    
+    
+    Stash::prepare(PSTR("POST /api/tickets HTTP/1.1" 
+                        "\r\n"
+                        "Host: $F" "\r\n"
+                        "User-Agent: Arduino/1.0" "\r\n"
+                        "Content-Length: $D" "\r\n"
+                        "Content-Type: application/x-www-form-urlencoded" "\r\n"
+                        "\r\n"
+                        "$H"),
+                        website, stash_size, sd);
+    
+    
+    
+    
+    // send the packet - this also releases all stash buffers once done
+    // Save the session ID so we can watch for it in the main loop.
+    session = ether.tcpSend();
+  }
+
+   const char* reply = ether.tcpReply(session);
+   if (reply != 0) {
+     res = 0;
+     Serial.println(reply);
+   }
+   delay(50);
+}
+
+
+
+void initialize_ethernet(void){
+  for(;;){ // keep trying until you succeed
+    //Reinitialize ethernet module
+    pinMode(5, OUTPUT);
+    Serial.println("Reseting Ethernet...");
+    digitalWrite(5, LOW);
+    delay(1000);
+    digitalWrite(5, HIGH);
+    delay(500);
+
+    // Change 'SS' to your Slave Select pin, if you arn't using the default pin
+    if (ether.begin(sizeof Ethernet::buffer, mymac, SS) == 0){
+      Serial.println( "Failed to access Ethernet controller");
+      continue;
+    }
+
+    if (!ether.dhcpSetup()){
+      Serial.println("DHCP failed");
+      continue;
+    }
+
+    ether.printIp("IP:  ", ether.myip);
+    ether.printIp("GW:  ", ether.gwip);
+    ether.printIp("DNS: ", ether.dnsip);
+
+    if (!ether.dnsLookup(website))
+      Serial.println("DNS failed");
+
+    ether.printIp("SRV: ", ether.hisip);
+
+    //reset init value
+    res = 0;
+    break;
+  }
+}
